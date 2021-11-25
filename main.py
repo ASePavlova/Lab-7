e7397c7b56fa37ee6813bb4c971a7d4e90e8cb12
ASePavlova <92172368+ASePavlova@users.noreply.github.com>
2021-11-25 20:44:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
 # Лабораторная работа 7
 # Даны списки, сортированные по возрастанию. Написать функцию, которая объединит списки в один отсортированный список и вернёт его.
-def rot(num):
-    str_num = str(num)
-    length = len(str_num)
-    for u in range(0, length):
-        if str_num[u] == '6':
-            num = num - 6 * (10 ** (length - u - 1)) + 9 * (10 ** (length - u - 1))
-            return num
-    return num
+
